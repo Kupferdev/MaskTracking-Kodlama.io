e7646d30696e0e2e5de02e2f1824ff931b9e0e09
AhmetBakır <bakir0438@gmail.com>
2024-02-06 21:39:58 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,14 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Entities.Concrete;
+
+Console.WriteLine("Hello World!");
+
+Person person1 = new();
+person1.FirstName = "Ahmet";
+person1.LastName = "Bakır";
+person1.DateOfBirthYear = 2004;
+person1.NationalId = 24815739606;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
